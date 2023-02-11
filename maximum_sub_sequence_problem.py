9735f4effd05ee3598e377303604eca0033453fe
MlMlR <111110044+MlMlR@users.noreply.github.com>
2023-02-11 13:49:21 +0100

    add kadanes_algorithm

--- maximum_sub_sequence_problem.py
+++ maximum_sub_sequence_problem.py
@@ -6,8 +6,6 @@ input = [31, -41, 59, 26, -53, 58, 97, -93, -23]
 
 def alg1(list):
     ll =len(list)
-    si = 0
-    ei = 0
     max_win = 0
     for i in range(ll):
         for j in range(i, ll):
@@ -19,12 +17,19 @@ def alg1(list):
 def list_comprehention(list):
     print(max([0] + [sum(list[s:e + 1]) for s in range(len(list)) for e in range(s, len(list))]))
 
-
+def kadanes_algorithm(list):
+    max = so_far = 0
+    for i in range(len(list)):
+        so_far = max(0, so_far + list[i])
+        max = max(max, so_far)
+    print(max)
+    
+    
 if __name__ == '__main__':
-    for i in range(5):
+    for i in range(10):
         sample = [random.randint(-100, 100) for _ in range(9*2**i)]
         print (9*2**i)
         start = t.time()
-        list_comprehention(sample)
+        kadanes_algorithm(input)
         end = t.time()
         print(end-start)
